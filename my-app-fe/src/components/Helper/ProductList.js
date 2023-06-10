d7a87d0f75535906f61810e27180b110fb03c805
albertM19 <albert.mallillin19@gmail.com>
2023-06-10 09:37:23 +0800

    initial commit

---
+++ my-app-fe/src/components/Helper/ProductList.js
@@ -0,0 +1,34 @@
+import '../../assets/images'
+
+function getProduct() {
+    const products = [
+        {
+            "id": 1,
+            "category": "",
+            "type": "Wallet",
+            "product_name": "Minimal Fold Over Wallet - Black",
+            "descriptions": "100% Full grain leather, 3 card pockets (Holds 1-2 cards each), 1 interior bill slot which holds multiple bills, Hand stitched for durability, Burnished and waxed edges, Ages better through time and use.",
+            "price": "PHP. 1,399",
+            "price_int": 1399,
+            "image_1": "",
+            "image_2": "",
+            "image_3": "",
+            "image_4": "",
+            "promo": "NO"
+        },
+        {
+            "id": 1,
+            "category": "Women",
+            "type": "Active Wear",
+            "product_name": "Performance Joggers",
+            "descriptions": "Give your work-from-home uniform a cozy upgrade with these soft  relaxed joggers that are also stylish enough for errands.",
+            "price": "PHP. 5,524.64",
+            "price_int": 5524.64,
+            "image_1": "https:\/\/n.nordstrommedia.com\/id\/sr3\/e52b3d19-7611-4b25-9342-f46ce7a75975.jpeg?crop=pad&amp;pad_color=FFF&amp;format=jpeg&amp;w=780&amp;h=1196&amp;dpr=2",
+            "image_2": "https:\/\/n.nordstrommedia.com\/id\/sr3\/4429bf87-78b1-4655-852b-e3dee70cc62e.jpeg?crop=pad&amp;pad_color=FFF&amp;format=jpeg&amp;w=780&amp;h=1196&amp;dpr=2",
+            "image_3": "https:\/\/n.nordstrommedia.com\/id\/sr3\/7e816f0f-980d-4546-b2a2-0f4de1ded8de.jpeg?crop=pad&amp;pad_color=FFF&amp;format=jpeg&amp;w=780&amp;h=1196&amp;dpr=2",
+            "image_4": "https:\/\/n.nordstrommedia.com\/id\/sr3\/0be958a6-2ced-4905-a8b5-b7ac8a408e85.jpeg?crop=pad&amp;pad_color=FFF&amp;format=jpeg&amp;w=780&amp;h=1196&amp;dpr=2",
+            "promo": "YES"
+        }
+    ]
+}
---
+++ my-app-fe/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Navbar, Image, Container, Nav, Form, Button } from 'react-bootstrap';
+import '../Home/Home.css';
+
+function Home() {
+    return (
+        <>
+            <Navbar bg="light" variant="light">
+                <Container fluid>
+                    <Navbar.Brand href="#home">
+                        <Image src={`/img/store_logo.png`} className='rounded-circle' width={70} height={70} />
+                    </Navbar.Brand>
+                    <Nav className="ms-auto">
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default Home;
